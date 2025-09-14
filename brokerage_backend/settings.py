864cbedaa1f76403d7c1a5f16bfb36a80db327a9
Cute Web <cutewebhosting@yahoo.com>
2025-09-14 19:59:00 +0100

    Add Render domain to ALLOWED_HOSTS

--- brokerage_backend/settings.py
+++ brokerage_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["tronexi.onrender.com", "tronexi.com", "www.tronexi.com", "localhost", "127.0.0.1"]
 
 # Application definition
 INSTALLED_APPS = [
