609c252adff24e460aad15feaf92f93c39c7345f
Cute Web <cutewebhosting@yahoo.com>
2025-09-15 21:29:05 +0100

    Switch database to PostgreSQL on Render

--- brokerage_backend/settings.py
+++ brokerage_backend/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,10 +85,9 @@ WSGI_APPLICATION = 'brokerage_backend.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # Password validation
